0f6aca48c53a2d5f686d4cbb767d395903be3fe3
lazyprogramming <61868181+lazyprogramming@users.noreply.github.com>
2020-03-06 12:25:27 +0200

    change directories

--- HTR-ImageImport/mainImage.py
+++ HTR-ImageImport/mainImage.py
@@ -3,18 +3,18 @@ import cv2
 from mainText import prepareImages
 from wordsegment import wordSegmentation, prepareImg
 
-prepareImages()
+#prepareImages()
 
 def main():
     """reads images from data/ and outputs the word-segmentation to out/"""
 
     # read input images from 'in' directory
-    imgFiles = os.listdir('../Images/')
+    imgFiles = os.listdir('Images/samples/')
     for (i, f) in enumerate(imgFiles):
-        print('Segmenting words of sample %s' % f)
+        print('Segmenting words of sample %s' %f)
 
         # read image, prepare it by resizing it to fixed height and converting it to grayscale
-        img = prepareImg(cv2.imread('../Images/%s' % f), 50)
+        img = prepareImg(cv2.imread('Images/samples/%s' %f), 50)
 
         # execute segmentation with given parameters
         # -kernelSize: size of filter kernel (odd integer)
@@ -24,19 +24,19 @@ def main():
         res = wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=100)
 
         # write output to 'out/inputFileName' directory
-        if not os.path.exists('../out/%s' % f):
-            os.mkdir('../out/%s' % f)
+        if not os.path.exists('out/%s' % f):
+            os.mkdir('out/%s' % f)
 
         # iterate over all segmented words
         print('Segmented into %d words' % len(res))
         for (j, w) in enumerate(res):
             (wordBox, wordImg) = w
             (x, y, w, h) = wordBox
-            cv2.imwrite('../out/%s/%d.png' % (f, j), wordImg)  # save word
+            cv2.imwrite('out/%s/%d.png' % (f, j), wordImg)  # save word
             cv2.rectangle(img, (x, y), (x + w, y + h), 0, 1)  # draw bounding box in summary image
 
         # output summary image with bounding boxes around words
-        cv2.imwrite('../out/%s/summary.png' % f, img)
+        cv2.imwrite('out/%s/summary.png' % f, img)
 
 
 if __name__ == '__main__':
